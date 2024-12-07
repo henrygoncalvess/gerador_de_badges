2b1ffa84e20a90920d4576a8f53fd3306baff6f8
henrygoncalvess <octanebt@gmail.com>
2024-12-07 14:55:05 -0300

    ajustando tamanho e espaçamento de inputs

---
+++ script.js
@@ -0,0 +1,8 @@
+var logo = document.querySelectorAll('input.logo')[0]
+var primeiraMensagem = document.querySelectorAll('input.msg1')[0]
+var segundaMensagem = document.querySelectorAll('input.msg2')[0]
+
+var corDalogo = document.querySelectorAll('input.logo')[1]
+var fundoPrimeiraMensagem = document.querySelectorAll('input.msg1')[1]
+var fundoSegundaMensagem = document.querySelectorAll('input.msg2')[1]
+
